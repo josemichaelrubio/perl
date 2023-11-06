3eea50ba262b116401f9a6747d893001156d516d
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-06 08:23:20 -0500

    Added download_html.pl

---
+++ download_html.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+
+use LWP::UserAgent;
+use IO::Socket::SSL;
+
+my $ua = LWP::UserAgent->new(cookie_jar=>{});
+
+$ua->ssl_opts(
+    'SSL_verify_mode' => IO::Socket::SSL::SSL_VERIFY_NONE, 
+    'verify_hostname' => 0
+);
+
+my $request = new HTTP::Request('GET', 'https://caveofpython.com/');
+
+my $response = $ua->request($request);
+
+unless($response->is_success()) {
+    die $response->status_line();
+}
+
+my $content = $response->decoded_content();
+
+print($content);
+
+print("Completed")
