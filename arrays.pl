8c0bf0b1c50dc2f54b88e2ea598fbd6d1bafb302
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-06 09:25:22 -0500

    In array.pl, replaced the if the single file is in location to a foreach which interates through the @files array

--- arrays.pl
+++ arrays.pl
@@ -1,18 +1,26 @@
 use strict;
 use warnings;
+$|=1;
 
 sub main{
 
+    my @files = (
+        '/Users/josemichaelrubio/Programs/perl/projects/robot.png',
+        '/Users/josemichaelrubio/Programs/perl/projects/helloworld.pl',
+        '/Users/josemichaelrubio/Programs/perl/projects/missing.txt'
+    );
 
-
-    my $file = '/Users/josemichaelrubio/Programs/perl/projects/robot.png';
-
-    if(-f $file ){
+    foreach my $file(@files){
+        if(-f $file ){
         print "Found file: $file\n";
     }
     else{
         print "File not found: $file\n";
     }
+
+    }
+
+
 }
 
 main();
