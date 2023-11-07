499970b5e727aa313f9896cd75a40f23e3ab7373
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-07 08:18:11 -0500

    writing_replacing_text.pl can now search for a specific word in a text then print the lines which contain the word in output.txt

--- writing_replacing_text.pl
+++ writing_replacing_text.pl
@@ -3,10 +3,22 @@ use warnings;
 $|=1;
 
 sub main {
+    my $input = '/Users/josemichaelrubio/Programs/perl/projects/inferno.txt';
+
+    open(INPUT, $input) or die("input file $input not found.\n");
+
     my $output = 'output.txt';
 
     open(OUTPUT, '>'.$output) or die "Can't create $output.\n";
-    print OUTPUT "hello\n";
+
+    while(my $line = <INPUT>){
+        if($line =~/ \bfire\b /){
+            print OUTPUT $line;
+
+        }
+
+    }
+    close(INPUT);
     close(OUTPUT);
 
 }
