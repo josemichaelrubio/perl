f5d2ef60ae2c3d60fb65ca6927c5793b7b8ed3ee
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-08 11:51:28 -0500

    worked on numberic_quantifiers.pl

--- numeric_quantifiers.pl
+++ numeric_quantifiers.pl
@@ -3,6 +3,18 @@ use warnings;
 $|=1;
 
 sub main {
-    print "Hello World";
+    # * zero or more of the preceeding character, as many as possible
+    # + one or more of the preceeding character, as many as possible
+    # *? Zero or more of the preceding character, as few as possible
+    # +? one or more of the preceeding, as few as possible
+    # {5} five of the preceding 
+    # {3,6} at lease 3 and at most 5
+
+
+my $text = 'DE$12388';
+
+    if($text=~ /(DE\$\d{3,})/){
+        print("Matched: '$1'\n");
+    }
 }
 main();
