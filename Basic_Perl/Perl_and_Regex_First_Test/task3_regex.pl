6cbb9a3fff0b9120f35190c386aa20cb15da9b46
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-13 09:33:29 -0500

    Added tasks for task3

--- Basic_Perl/Perl_and_Regex_First_Test/task3_regex.pl
+++ Basic_Perl/Perl_and_Regex_First_Test/task3_regex.pl
@@ -1,8 +1,18 @@
+=begin comment
+Task 3:
+	• Declare string variable
+	• Write a regular expression that can pick out machine code from text
+		○ It has two alphanumeric characters
+		○ Followed by 4 digits
+	• Print machine code in the text or "not found" if there are no matches
+=end comment
+
+=cut
 use strict;
 use warnings;
 $|=1;
 
 sub main {
-    print "Hello World";
+    
 }
 main();
