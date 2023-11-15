bfa20191e4cd49517bc5ee6b1fff751bf4826562
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-15 07:55:35 -0500

    organize directories and created 2-reading_files_Line_by_Line

---
+++ 1-Basic_Perl/Perl_and_Regex_First_Test/task4_regex.pl
@@ -0,0 +1,24 @@
+=begin comment
+Task 4:
+	• Only match two alphanumeric and four digits
+=end comment
+
+=cut
+
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+    my $text = "The code for this device is GP8767.";
+
+        if($text=~/(\b\w{2}\d{4}\b)/){
+            print ("$1\n");
+        }
+        else {
+            print ("Code not found\n");
+        }
+    }
+
+main();
