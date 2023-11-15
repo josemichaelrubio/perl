8e8b63c13e30961363935d4653a5bcfc7052b591
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-15 07:29:12 -0500

    Completed Task 3

--- Basic_Perl/Perl_and_Regex_First_Test/task3_regex.pl
+++ Basic_Perl/Perl_and_Regex_First_Test/task3_regex.pl
@@ -8,11 +8,20 @@ Task 3:
 =end comment
 
 =cut
+
 use strict;
 use warnings;
+
 $|=1;
 
 sub main {
-    
-}
+    my $text = "The code for this device is GP8767.";
+        if($text=~/(\w{2}\d{4})/){
+            print ("$1\n");
+        }
+        else {
+            print ("Code not found\n");
+        }
+    }
+
 main();
