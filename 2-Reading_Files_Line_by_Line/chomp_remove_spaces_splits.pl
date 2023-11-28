6361e84dc67e35d1317f7dad9a253e108ef3bcee
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-11-28 07:51:48 -0500

    Created chomp_remove_spaces_split.pl

---
+++ 2-Reading_Files_Line_by_Line/chomp_remove_spaces_splits.pl
@@ -0,0 +1,28 @@
+=begin comment
+
+=end comment
+
+=cut
+
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+  my $input = 'test.csv';
+  
+  unless(open(INPUT, $input)){
+    die "\n Cannot open $input\n";
+  }
+  <INPUT>;
+
+  while(my $line = <INPUT>){
+    my @values= split(',',$line);
+    print "$values[0]\n";
+  }
+
+  close(INPUT);
+}
+
+main();
