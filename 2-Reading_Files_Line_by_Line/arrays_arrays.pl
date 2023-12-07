7c1a541c0415eedacb42e73564012354473802ba
Jose Michael Rubio <contact@josemichaelrubio.com>
2023-12-07 08:13:46 -0500

    Created arrays_arrays.pl

---
+++ 2-Reading_Files_Line_by_Line/arrays_arrays.pl
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+
+#use Data::Dumper;
+
+$|=1;
+
+sub main {
+  my $input = 'test.csv';
+  
+  unless(open(INPUT, $input)){
+    die "\n Cannot open $input\n";
+  }
+  <INPUT>;
+
+  my @lines;
+
+  while(my $line = <INPUT>){
+    chomp $line;
+    #print "'$line'\n";
+    my @values= split(/\s*,\s*/,$line);
+    #print "$values[0]\n";
+    #print Dumper(@values);
+
+    push @lines, $line;
+  }
+
+   close(INPUT);
+
+  foreach my $line(@lines){
+    print $line . "\n";
+  }
+}
+
+main();
