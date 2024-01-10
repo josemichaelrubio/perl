cd5bd1c2b1b74a905f1ba58b6fdcf57e1602c5d2
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-01-10 15:40:24 -0500

    completed _20-iterating_over_hashes.pl

--- 2-Reading_Files_Line_by_Line/_20-iterating_over_hashes.pl
+++ 2-Reading_Files_Line_by_Line/_20-iterating_over_hashes.pl
@@ -10,6 +10,42 @@ use warnings;
 $|=1;
 
 sub main {
-    print "Hello World";
+    my %foods = (
+        "mice" => "milk",
+        "dogs" => "meat",
+        "birds" => "seeds",
+    );
+
+    # We can define an array of variables like this:
+    my ($one, $two, $three) = (19,72,113);
+
+# Method 1: 
+## gettings key then value as you need them
+## Most efficent
+    while( my ($key, $value)= each %foods){
+        print "$key: $value\n";
+    };
+
+# Method 2:
+## Getting all the keys at once
+## It's slower, especially for big hashes, but it helps to sort the hash )
+    
+    my @animals = keys %foods;
+    
+    foreach my $key(@animals){
+        my $value = $foods{$key};
+        print "$key = $value\n";
+    }
+
+# Method 2.b:
+## Same output but it's quicker.
+## also includes the sort keyword, which can be use above as well
+
+    foreach my $key(sort keys %foods){
+        my $value = $foods{$key};
+        print "$key = $value\n";
+    }
+
 }
+
 main();
