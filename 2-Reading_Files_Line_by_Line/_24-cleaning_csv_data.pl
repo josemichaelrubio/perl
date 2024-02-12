177896c8e13e843c17151efc07c54ec67c4493cf
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-02-12 12:52:16 -0500

    adding the notes for _24-cleaning_csv_data.pl

--- 2-Reading_Files_Line_by_Line/_24-cleaning_csv_data.pl
+++ 2-Reading_Files_Line_by_Line/_24-cleaning_csv_data.pl
@@ -1,5 +1,36 @@
 =begin comment
+Cleaning CSV Data
 
+Specifying alternatives in regular expressions then use that to clean up CSV data
+
+One common problem with CSV files is often we get space at the beginning or end of a line. Thus we have to clean the line BEFORE we process it.
+        $line=~ s/<get rid>/<replace with>/;
+	• We put stuff in here that we want to replace
+
+Cleaning up the beginning of a line: 
+To specify the beginning of a line in regular expression, we use ^
+Thus:
+        $line=~ s/^\s*//;
+	• Here we gather all the spaces in the beginning of the line and replace it with nothing.
+
+Cleaning up the end of a line:
+To specify the end of a line in a regular expression, we use $
+Thus: 
+        $line =~ s/\s*$//;
+	• Here we gather all the spaces at the end of the line and replace it within nothing
+
+Cleaning up the beginning or the end of the line:
+Here we use all that was mentioned above and specify one or the other alternatives by using the |
+Thus:
+        $line =~ s/^\s*|\s*$//;
+
+Cleaning up both beginning and the end of the line:
+We use the all that was mentioned above and the g (global replace, replace as mush as possible) at the end of the expression:
+        $line =~ s/^\s*|\s*$//g;
+
+Cleaning up random characters within the value:
+Thus:
+        $line =~ s/\?|approx\.|\$//g;
 =end comment
 
 =cut
