ec68898dce3bdde65740bcf65ee1a313f6d1227c
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-02-27 13:55:15 -0500

    Completed _5-collecting_repeated_matches_all_at_once.pl

--- 3-Web_Scraping_and_More_Regular_Expressions/_5-collecting_repeated_matches_all_at_once.pl
+++ 3-Web_Scraping_and_More_Regular_Expressions/_5-collecting_repeated_matches_all_at_once.pl
@@ -22,7 +22,23 @@ sub main {
     }
     my $content = $response->decoded_content;
 
-    
+    while($content=~ //g){
+        # trawls through your data and find one match after another
+        # memory efficent
+        # however, you must write one while loop after another to gather the things you need
+    }
+
+        # use a regular expression in an array context, it returns all the things that it matched
+    my @classes = $content =~ m|class="([^"']*?)"|ig;
+        # Note: using a regualr expression in a if or while loop context, it is treated as a true or false
+        # Using an array in a scalar context
+    if(@classes == 0){
+        print "No matches found\n";
+    } else {
+        foreach my $class(@classes){
+            print $class . "\n";
+        }
+    }
     
 }
 
