bbee44a3269d301a4cbd702f909d45d5bc31083e
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-02-28 10:21:48 -0500

    completed _3-multi_line_strings_and_comments.pl

--- 4-Building_a_Complete_Program-Command_Line_Options/_3-multi_line_strings_and_comments.pl
+++ 4-Building_a_Complete_Program-Command_Line_Options/_3-multi_line_strings_and_comments.pl
@@ -1,5 +1,10 @@
 =begin comment
+Sub usage print a nice message when check usage is false
 
+multi-line string
+	• << IDENTIFIER ;
+IDENTIFIER
+to end a multi-line string, end with the identifier with in no indent or ;
 =end comment
 
 =cut
@@ -11,6 +16,13 @@ use Data::Dumper;
 use Getopt::Std;
 
 $|=1;
+
+# Muli-line comment
+=pod
+Pod means "plain old documentation"
+=cut
+
+# single line comment
 
 sub main {
 
@@ -28,7 +40,27 @@ sub checkusage{
 }
 
 sub usage{
-    print "incorrect usage\n";
+
+    # defining a multi-line string
+    #print <<USAGE;
+    # or
+    my $help = <<USAGE;
+
+    usage: perl _3-multi_line_strings_and_comments.pl -f <file name> -a -c
+        -f <file name>: specify xml file name to parse
+        -a turn off error checking
+        -c turn on feature c
+    example usage:
+        perl _3-multi_line_strings_and_comments.pl -f test.xml -a -c
+
+USAGE
+# ends multi-line string
+#* must be at the beginning of the line
+
+    die $help;
+    # or
+    # exit();
+
 }
 
 main();
