b44a4d1994a7eab4e58a35b4bea9140d227f7aba
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-02-28 12:13:16 -0500

    Completed _6-checking_values_in_hashes.pl

--- 4-Building_a_Complete_Program-Command_Line_Options/_6-checking_values_in_hashes.pl
+++ 4-Building_a_Complete_Program-Command_Line_Options/_6-checking_values_in_hashes.pl
@@ -15,28 +15,42 @@ sub main {
 
     my %opts;
 
-    getopts('af:c', \%opts);
+    getopts('af:r', \%opts);
 
     if(!checkusage(\%opts)){
         usage();
     }
 
 =pod
-
+    perl _5-references_to_hashes.pl -f test.xml -a -r
+    Key value pairs:
+        a = 1
+        r = 1
+        f = test.xml
 =cut
 
-    my $opts_ref = \%opts;
-
-    print $opts{'f'} . "\n";
-
-    print $opts_ref->{'f'} . "\n";
 }
 
 sub checkusage{
  
     my $opts_ref = shift;
 
-    print $opts_ref->{'f'} . "\n";
+    my $a = $opts_ref->{"a"};
+    my $f = $opts_ref->{"f"};
+    my $r = $opts_ref->{"r"};
+
+    # Imagine a is optional: if it is not there, it will be undef
+    # r is mandatory: it means run the program
+    # f is mandatory: it means file name
+
+    unless(defined($r) and defined($f)){
+        return 0;
+    }
+
+    unless($f=~/\.xml$/){
+        print "File must have an .xml extension\n";
+        return 0;
+    }
 
     return 1;
 }
@@ -48,9 +62,9 @@ sub usage{
     usage: perl _3-multi_line_strings_and_comments.pl -f <file name> -a -c
         -f <file name>: specify xml file name to parse
         -a turn off error checking
-        -c turn on feature c
+        -r run the program
     example usage:
-        perl _3-multi_line_strings_and_comments.pl -f test.xml -a -c
+        perl _3-multi_line_strings_and_comments.pl -f test.xml -a -r
 
 USAGE
 
