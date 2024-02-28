7bad62d9b376d5f839a625e44a8f37fbd6114588
Jose Michael Rubio <contact@josemichaelrubio.com>
2024-02-28 08:17:02 -0500

    completed _1-getting_command_line_options.pl

--- 4-Building_a_Complete_Program-Command_Line_Options/_1-getting_command_line_options.pl
+++ 4-Building_a_Complete_Program-Command_Line_Options/_1-getting_command_line_options.pl
@@ -1,5 +1,17 @@
 =begin comment
+Getting Command Line Options
+----------------------------
+A Program to Parse XML code
 
+In Perl, All the command line arguments come through in an array called ARGV
+
+Perl Command-Line Options
+
+use Getopt:: std;
+	• enables flexible argument retrieval
+	• You need a hash with your arguments that will be filled with key value pairs
+		○ keys are flags
+values are optional arguments to those flags
 =end comment
 
 =cut
@@ -7,12 +19,24 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+use Getopt::Std;
+
 $|=1;
 
 sub main {
 
-    print "Hello World\n";
+    print Dumper(@ARGV);
+    print $ARGV[0] . "\n";
 
+    my %opts;
+
+    # a,f, and c are arbitrary flags
+    # : indicates that f takes in an arguement
+    getopts('af:c', \%opts);
+    print Dumper(%opts);
+    my $file = $opts{'f'};
+    print "File: $file\n";
 }
 
 main();
